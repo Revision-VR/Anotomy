65e5d207cfeefbc43684ab940cb6ff2aab426675
Abror <nabiddinovabror8@gmail.com>
2024-02-22 11:34:14 +0500

    Update animation controller

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,22 +1,27 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    public Animator animator;
+    public GameObject Button1;
+    public GameObject Button2;
+   
 
     public void SceneAlmashuv()
     {
+        //animator.Play("HumanSceneAlmashuv");
+        animator.SetInteger("Human", 5);
+        Button1.SetActive(false);
+        Button2.SetActive(false);
+        StartCoroutine(kutish());
+      
+    }
+
+    IEnumerator kutish()
+    {
+        yield return new WaitForSeconds(2);
         SceneManager.LoadScene(2);
     }
 }
