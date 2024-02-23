a7e6844dd2f74f1c1d53f69187e34538c6c0743c
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-02-23 22:12:31 +0500

    Update AnatomiyaOrgans scene

---
+++ Assets/Scripts/TestSceneChange.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TestSceneChange : MonoBehaviour
+{
+    public Animator changeAnim;
+    public GameObject TurnOfAnim;
+
+    public void Click()
+    {
+        StartCoroutine(AnimationController());
+    }
+
+    private IEnumerator AnimationController()
+    {
+        TurnOfAnim.SetActive(true);
+        changeAnim.Play("CanvasAnim");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(4);
+
+    }
+}
