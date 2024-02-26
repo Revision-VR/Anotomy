6fd5726792f0a92fec40f06bc0f0fd578ac1de3f
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-02-26 14:32:45 +0500

    Update HumanBodyController script

--- Assets/Scripts/Anatomiya/HumanBodyController.cs
+++ Assets/Scripts/Anatomiya/HumanBodyController.cs
@@ -26,6 +26,11 @@ public class HumanBodyController : MonoBehaviour
 
     string _language;
 
+    private void Start()
+    {
+        _language = PlayerPrefs.GetString("Language");
+    }
+
     public void PlayAnimations(int index)
     {
         if (_bodyButtons.activeSelf  && index == 5)
@@ -41,33 +46,32 @@ public class HumanBodyController : MonoBehaviour
                 case "uz":
                     _bodInfoNameText.text = _bodyInfoName[index - 1];
                     _bodInfoText.text = _bodyInfo[index - 1];
+                    print(1);
                     _bodyButtons.SetActive(false);
                     break;
 
                 case "ru":
                     _bodInfoNameText.text = _bodyInfoNameRu[index - 1];
                     _bodInfoText.text = _bodyInfoRu[index - 1];
+                    print(1);
+
                     _bodyButtons.SetActive(false);
                     break;
 
                 case "en":
                     _bodInfoNameText.text = _bodyInfoNameEn[index - 1];
                     _bodInfoText.text = _bodyInfoEn[index - 1];
+                    print(1);
+
                     _bodyButtons.SetActive(false);
                     break;
             }
-
-
-
-
             
         }
         else
         {
             _bodyButtons.SetActive(true);
         }
-        
-
 
         _bodyAnimations.SetInteger("HumanBody", index);
     }
